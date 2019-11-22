713df3ba0db61159c96301f6dce1e3ed91bdb332
Wesley Maddox <wm326@cornell.edu>
2019-11-22 17:14:49 -0500

    wip for batching using dirichlet

--- kernels/cnn_kernel.py
+++ kernels/cnn_kernel.py
@@ -1,6 +1,7 @@
 import gpytorch
 
 from gpytorch.kernels import Kernel
+from gpytorch.lazy import BatchRepeatLazyTensor, lazify
 
 class CNNGP_Kernel(Kernel):
     def __init__(self, model, shape, **kwargs):
@@ -12,17 +13,25 @@ class CNNGP_Kernel(Kernel):
     def forward(self, x1, x2=None, diag=False, last_dim_is_batch=False, **params):
         # TODO: figure out how to have multiple outputs - i believe method in 
         # def num_ouptuts_per_input -> but also for the kernel model itself
-        
+
+        print(self.batch_shape, x2 is None)        
         if len(x1.shape) is 2:
             x1 = x1.view(-1, *self.shape)
 
         # TODO: figure out how to have batched dimensional outputs
         if x2 is None:
-            return self.model(x1)
+            kernel = lazify(self.model(x1, diag=diag))
         else:
             x2 = x2.view(-1, *self.shape)
+            kernel = lazify(self.model(x1, x2, diag=diag))
 
-        return self.model(x1, x2, diag=diag)
+        print('shape of kernel is: ', kernel.shape)
+        res = BatchRepeatLazyTensor(kernel, batch_repeat=self.batch_shape)
+
+        if last_dim_is_batch:
+            res = res.permute(1, 2, 0)
+
+        return res
 
     
 
